073223fbdf72842e58ad30f103c716ac73362b58
webandgis <67840139+webandgis@users.noreply.github.com>
2020-08-11 19:16:48 -0300

    Add files via upload

---
+++ bye.js
@@ -0,0 +1,9 @@
+(function (window) {
+    var speakWord= "Goodbye";
+    var byeSpeaker= function (name){
+      console.log( speakWord + ""+ name);
+    }
+  
+    windowbyeSpeaker = byeSpeaker;
+  
+  })(window);
